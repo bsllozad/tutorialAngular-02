0e68610044b8876c58cd49073767cec6ec418f35
Bernardo Steven Lopez Lozada <Bernardo.Lopez@OMNICON.CC>
2019-12-27 11:35:39 -0500

    COntinuacion del curso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
   { path: '**', component: HomeComponent }
 ];
 
